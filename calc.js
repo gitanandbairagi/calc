fa0e2ffe8cf9c40f0a9869f08cc087e83d624c59
Anand Bairagi <anandbairagi500@gmail.com>
2022-04-10 00:14:57 +0530

    fourth commit

--- calc.js
+++ calc.js
@@ -1,21 +1,26 @@
 let screen = document.getElementById("screen")
+let history = document.getElementById("history")
 buttons = document.querySelectorAll("button")
 let screen_value = ""
 
 for(item of buttons) {
     item.addEventListener("click", (e) => {
         buttonText = e.target.innerHTML
-        if (buttonText == "X") {
+        let past
+        if (buttonText == "x") {
             buttonText = "*"
             screen_value += buttonText
             screen.value = screen_value
         }
-        else if (buttonText == "C") {
+        else if (buttonText == "AC") {
             screen_value = ""
             screen.value = screen_value
         }
         else if (buttonText == "=") {
+            past = screen.value
             screen.value = eval(screen_value)
+            past += "=" + screen.value
+            history.value = past
         }
         else {
             screen_value += buttonText
