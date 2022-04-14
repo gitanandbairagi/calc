33bb794e1a12b05e055e824a92d78cc3f45cff2a
Anand Bairagi <anandbairagi500@gmail.com>
2022-04-14 12:23:08 +0530

    u

--- js/mode.js
+++ js/mode.js
@@ -1,4 +1,19 @@
 let mode = document.getElementById("mode")
+let css_id = "cssId"
 mode.addEventListener("click", () => {
-    mode.classList
+    console.log(document.getElementById("cssId"))
+    if (! document.getElementById("cssId")) {
+        let head = document.getElementsByTagName("head")[0]
+        let link = document.createElement("link")
+        link.id = "cssId"
+        link.rel = "stylesheet"
+        link.type = "text/css"
+        link.href = "css/lightMode.css"
+        head.append(link)
+        console.log("IF")
+    }
+    else {
+        document.getElementById("cssId").remove()
+        console.log("ELSE")
+    }
 }) 
